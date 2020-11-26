f5c91fac0c51c1efe2a4f5180f0996ca8c99e08c
Miqraro <miqraro@gmail.com>
2020-11-26 21:09:49 +0100

    Update load SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tarea(id, descripcion, puntos) VALUES (nextval('hibernate_sequence'), 'Hacer los déberes', 1);
+INSERT INTO tarea(id, descripcion, puntos) VALUES (nextval('hibernate_sequence'), 'Recoger la mesa', 2);
+INSERT INTO tarea(id, descripcion, puntos) VALUES (nextval('hibernate_sequence'), 'Lavarse los dientes', 1);
